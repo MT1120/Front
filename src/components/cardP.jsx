06cf5dde913a87ac00784b943371f80a2045c5a5
MT1120 <mateoanaya6@gmail.com>
2023-10-31 18:33:45 -0500

    Completo

--- src/components/cardP.jsx
+++ src/components/cardP.jsx
@@ -16,9 +16,65 @@ export default function cardP() {
             <li class="list-group-item">Telefono: XXXXXXXXX</li>
             <li class="list-group-item">Genero: XXXXXXXXX</li>
         </ul>
-        <div class="card-footer text-body-secondary">
-            <a href="/" class="btn btn-primary">Go somewhere</a>
+        <div class="card-footer text-center text-body-secondary">
+            <a href="/" class="btn btn-primary">Regresar</a>
         </div>
+        <form class="row g-3 mt-5" role="search">
+        
+            <div class="col-md-6 mt-5 ">
+                <input type="text" class="form-control" placeholder="Primer Nombre" aria-label="First name"/>
+            </div>
+            <div class="col-md-6 mt-5">
+                <input type="text" class="form-control" placeholder="Segundo Nombre" aria-label="Last name"/>
+            </div>
+            
+            <div class="col-md-6 mt-5">
+                <input type="text" class="form-control" placeholder="Apellido" aria-label="First name"/>
+            </div>
+            <div class="col-md-6 mt-5">
+                <label class="visually-hidden" for="autoSizingInputGroup">Correo</label>
+                <div class="input-group">
+                    <div class="input-group-text">@</div>
+                    <input type="email" class="form-control" id="autoSizingInputGroup" placeholder="Correo"/>
+                </div>
+            </div>
+            <div class="col-md-6 mt-5">
+                <label class="visually-hidden" for="autoSizingInputGroup">ID</label>
+                <div class="input-group">
+                    <div class="input-group-text">ID</div>
+                    <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="ID"/>
+                </div>
+            </div>
+            <div class="col-md-6 mt-5">
+                <label class="visually-hidden" for="autoSizingInputGroup">Fecha</label>
+                <div class="input-group">
+                    <div class="input-group-text">Fecha</div>
+                    <input type="date" class="form-control" id="autoSizingInputGroup" placeholder="Fecha"/>
+                </div>
+            </div>
+
+            <div class="col-md-6 mt-5">
+                <label class="visually-hidden" for="autoSizingInputGroup">Celular</label>
+                <div class="input-group">
+                    <div class="input-group-text">#</div>
+                    <input type="text" class="form-control" id="autoSizingInputGroup" placeholder="Text"/>
+                </div>
+            </div>
+
+            <div class="col-md-6 mt-5">
+            
+                <select id="inputState" class="form-select">
+                    <option selected>Genero</option>
+                    <option>Masculino</option>
+                    <option>Femenino</option>
+                </select>
+            </div>
+
+            <div class="d-grid gap-2 mt-5">
+                <button type="submit" class="btn btn-primary">Editar</button>
+            </div>
+        
+      </form>
     </div>
   )
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,77 +1,98 @@
 import React from 'react'
 import { FaUser } from "react-icons/fa"
+import { FaTrash } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 
-export default function home() {
-  return (
-    <div>
 
-        <table className="table table-hover mt-5">
-            <thead>{/*<tr className="table-primary">*/}
-                <tr className="table-primary">
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Teléfono</th>
-                    <th scope= "col">id</th>
-                    <th scope="col">Fecha de nacimiento</th>
-                    <th scope="col">Género</th>
-                    <th scope="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td className='justify-content-center'>
-                        <button className="btn btn-danger btn-sm" >
-                            <FaUser/>
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td className='justify-content-center'>
-                        <button className="btn btn-danger btn-sm" >
-                            <FaUser/>
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td className='justify-content-center'>
-                        <button className="btn btn-danger btn-sm" >
-                            <FaUser/>
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td>Carlos</td>
-                    <td className='justify-content-center'>
-                        <button className="btn btn-danger btn-sm" >
-                            <FaUser/>
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-            </table>
-    </div>
-  )
+export default function Home() {
+
+    const navigate = useNavigate()
+
+    const handleNavigate = (event) => {
+        navigate('/cardP')
+    }
+    return (
+        <div>
+
+            <table className="table table-hover mt-5">
+                <thead>{/*<tr className="table-primary">*/}
+                    <tr className="table-primary">
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Teléfono</th>
+                        <th scope= "col">id</th>
+                        <th scope="col">Fecha de nacimiento</th>
+                        <th scope="col">Género</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td className='d-flex align-items-center justify-content-evenly'>
+                            <button className="btn btn-danger btn-sm" onClick={handleNavigate}>
+                                <FaUser/>
+                            </button>
+                            <button className="btn btn-danger btn-sm " >
+                                <FaTrash/>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td className='d-flex align-items-center justify-content-evenly'>
+                            <button className="btn btn-danger btn-sm" onClick={handleNavigate}>
+                                <FaUser/>
+                            </button>
+                            <button className="btn btn-danger btn-sm " >
+                                <FaTrash/>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td className='d-flex align-items-center justify-content-evenly'>
+                            <button className="btn btn-danger btn-sm" onClick={handleNavigate}>
+                                <FaUser/>
+                            </button>
+                            <button className="btn btn-danger btn-sm " >
+                                <FaTrash/>
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td>Carlos</td>
+                        <td className='d-flex align-items-center justify-content-evenly'>
+                            <button className="btn btn-danger btn-sm" onClick={handleNavigate}>
+                                <FaUser/>
+                            </button>
+                            <button className="btn btn-danger btn-sm " >
+                                <FaTrash/>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+                </table>
+        </div>
+    )
 }
