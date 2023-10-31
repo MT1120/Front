47d76c3f39d54cfc22b3ce5388c24e12cae8add6
MT1120 <mateoanaya6@gmail.com>
2023-10-31 18:51:50 -0500

    Cambios - Compleo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,34 +1,43 @@
 import React from 'react'
 import logo from '../components/assets/project-management.png'
+import { useNavigate } from 'react-router-dom'
 
 
-export default function navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg bg-secondary mb-4   p-0">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="/" >
-                <div className="d-flex">
-                    <img src={logo} alt="logo" className="mr-2"/>
-                    <div className='text-white fs-5'>Proyecto Final</div>
-                </div>
-            </a>
-            <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
-                
-                <ul class="nav ">
+export default function Navbar() {
+    
+    const navigate = useNavigate()
+
+    const handleNavigate = (event) => {
+        navigate('/addP')
+    }
+    return (
+        <nav className="navbar navbar-expand-lg bg-secondary mb-4   p-0">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/" >
+                    <div className="d-flex">
+                        <img src={logo} alt="logo" className="mr-2"/>
+                        <div className='text-white fs-5'>Proyecto Final</div>
+                    </div>
+                </a>
+                <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
                     
-                    <li class="nav-item">
-                            <a class="nav-link active text-white fs-5" href="/addP" aria-current="page">
-                                Crear Personas
-                            </a>
+                    <ul class="nav ">
                         
-                    </li>
-                </ul>
+                        <li class="nav-item">
+                                
+                                <button className='btn text-white fs-5' onClick={handleNavigate}>
+    
+                                    Crear Personas
+                                </button>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+                    <button class="btn btn-primary" type="submit">Buscar</button>
+                </form>
             </div>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
-                <button class="btn btn-primary" type="submit">Buscar</button>
-            </form>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
